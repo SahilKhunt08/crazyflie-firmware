d3a9065f269ae091cec9041e0b480fbe7aa3da89
Muhammad Arshad <63872178+MuhammadArshad123@users.noreply.github.com>
2025-04-12 16:37:26 -0500

    added outoftree controller
    - followed cf instructions to add controller
    -removed ifdef/endifs for config_controller_oot

--- src/modules/src/controller/controller.c
+++ src/modules/src/controller/controller.c
@@ -30,9 +30,7 @@ static ControllerFcns controllerFunctions[] = {
   {.init = controllerINDIInit, .test = controllerINDITest, .update = controllerINDI, .name = "INDI"},
   {.init = controllerBrescianiniInit, .test = controllerBrescianiniTest, .update = controllerBrescianini, .name = "Brescianini"},
   {.init = controllerLeeFirmwareInit, .test = controllerLeeFirmwareTest, .update = controllerLeeFirmware, .name = "Lee"},
-  #ifdef CONFIG_CONTROLLER_OOT
-  {.init = controllerOutOfTreeInit, .test = controllerOutOfTreeTest, .update = controllerOutOfTree, .name = "OutOfTree"},
-  #endif
+  {.init = controllerOutOfTreeInit, .test = controllerOutOfTreeTest, .update = controllerOutOfTree, .name = "OutOfTree"}
 };
 
 
